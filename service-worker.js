e700d3c1e29a9fc9627397c1be1d1a546cf8633e
hangyeol740 <hangyeol740@gmail.com>
2026-09-24 17:07:13 +0900

    게임최적화

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dawn-continent-v3';
+const CACHE_NAME = 'dawn-continent-v4';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
